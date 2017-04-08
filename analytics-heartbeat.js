3c7dadc1d00c7fcc15a1da4f21077091b6dcb67f
Mieszkania <mieszkania@users.noreply.github.com>
2017-04-08 21:04:59 +0200

    Wrap in anonymous functions.

--- analytics-heartbeat.js
+++ analytics-heartbeat.js
@@ -1,16 +1,18 @@
-var start_ms = Date.now();
-var delay_ms = 1000
-var multiplier = 1.1
+(function() {
+  var start_ms = Date.now();
+  var delay_ms = 1000
+  var multiplier = 1.1
 
-function sendHeartbeat() {
-  var seconds = Math.floor((Date.now() - start_ms) / 1000);
-  ga('send', 'event', 'Heartbeat', 'Thump', undefined, seconds);
+  function sendHeartbeat() {
+    var seconds = Math.floor((Date.now() - start_ms) / 1000);
+    ga('send', 'event', 'Heartbeat', 'Thump', undefined, seconds);
+    setHeartbeat();
+  }
+
+  function setHeartbeat() {
+    setTimeout(sendHeartbeat, delay_ms);
+    delay_ms *= multiplier;
+  }
+
   setHeartbeat();
-}
-
-function setHeartbeat() {
-  setTimeout(sendHeartbeat, delay_ms);
-  delay_ms *= multiplier;
-}
-
-setHeartbeat();
+})();
--- analytics-visibility.js
+++ analytics-visibility.js
@@ -1,37 +1,39 @@
-var elements = [];
+(function() {
+  var elements = [];
 
-function isVisible(element) {
-  var rect = element.getBoundingClientRect();
-  return rect.top < window.innerHeight &&
-         rect.left < window.innerWidth &&
-         rect.bottom > 0 &&
-         rect.right > 0;
-}
+  function isVisible(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.top < window.innerHeight &&
+           rect.left < window.innerWidth &&
+           rect.bottom > 0 &&
+           rect.right > 0;
+  }
 
-function reportVisible() {
-  elements = Array.prototype.filter.call(elements, function(element) {
-    if (isVisible(element)) {
-      ga('send', 'event', 'Visibility', element.id);
-      return false;
+  function reportVisible() {
+    elements = Array.prototype.filter.call(elements, function(element) {
+      if (isVisible(element)) {
+        ga('send', 'event', 'Visibility', element.id);
+        return false;
+      }
+      return true;
+    });
+    if (elements.length == 0) {
+      window.removeEventListener('resize', reportVisible);
+      window.removeEventListener('scroll', reportVisible);
     }
-    return true;
-  });
-  if (elements.length == 0) {
-    window.removeEventListener('resize', reportVisible);
-    window.removeEventListener('scroll', reportVisible);
   }
-}
 
-function findElements() {
-  elements = document.getElementsByClassName('report');
-  elements = Array.prototype.filter.call(elements, function(element) {
-    return !!element.id;
-  });
-  if (elements.length > 0) {
-    window.addEventListener('resize', reportVisible);
-    window.addEventListener('scroll', reportVisible);
-    reportVisible();
+  function findElements() {
+    elements = document.getElementsByClassName('report');
+    elements = Array.prototype.filter.call(elements, function(element) {
+      return !!element.id;
+    });
+    if (elements.length > 0) {
+      window.addEventListener('resize', reportVisible);
+      window.addEventListener('scroll', reportVisible);
+      reportVisible();
+    }
   }
-}
 
-window.addEventListener('DOMContentLoaded', findElements);
+  window.addEventListener('DOMContentLoaded', findElements);
+})();
