5d4fef53d9127bd397ae8a1fab44d6884e36cd5b
Mieszkania <mieszkania@users.noreply.github.com>
2017-05-01 22:29:17 +0200

    First attempt to time visibility.

--- analytics-visibility.js
+++ analytics-visibility.js
@@ -1,43 +1,63 @@
 (function() {
+  var lastIntegration;
+  var totalArea;
   var monitoredElements = [];
 
-  function isVisible(element) {
+  function visibleArea(element) {
+    if (document.hidden) {
+      return 0;
+    }
     var rect = element.getBoundingClientRect();
-    return rect.top < window.innerHeight &&
-           rect.left < window.innerWidth &&
-           rect.bottom > 0 &&
-           rect.right > 0;
+    var y = Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
+    if (y <= 0) {
+      return 0;
+    }
+    var x = Math.min(rect.right, window.innerWidth) - Math.max(rect.left, 0);
+    if (x <= 0) {
+      return 0;
+    }
+    return x * y;
   }
 
-  function checkElements(event) {
+  function integrate() {
+    var now = Date.now();
+    var elapsed = now - lastIntegration;
+    lastIntegration = now;
     for (var i = 0; i < monitoredElements.length; i++) {
       var monitor = monitoredElements[i];
-      var element = monitor.element;
-      var wasVisible = monitor.visible;
-      monitor.visible = isVisible(element);
-      if (!wasVisible && monitor.visible) {
-        ga('send', 'event', 'Visibility', 'Visible', {
-          eventLabel: element.id,
-          nonInteraction: !event,
-        });
+      if (monitor.area > 0) {
+        monitor.ms += elapsed * monitor.area / totalArea;
       }
     }
   }
 
+  function checkElements(event) {
+    integrate();
+    totalArea = 0;
+    for (var i = 0; i < monitoredElements.length; i++) {
+      var monitor = monitoredElements[i];
+      monitor.area = visibleArea(monitor.element);
+      totalArea += monitor.area;
+    }
+  }
+
   function findElements() {
-    allElements = document.getElementsByClassName('report-visibility');
+    var allElements = document.querySelectorAll('[data-timing]');
     for (var i = 0; i < allElements.length; i++) {
       var element = allElements[i];
       if (element.id) {
-        monitoredElements.push({element: element, visible: false});
+        monitoredElements.push({element: element, ms: 0});
       }
     }
     if (monitoredElements.length > 0) {
       window.addEventListener('resize', checkElements);
       window.addEventListener('scroll', checkElements);
+      document.addEventListener('visibilitychange', checkElements);
       checkElements();
     }
   }
 
   document.addEventListener('DOMContentLoaded', findElements);
+
+  window.bla = monitoredElements;
 })();
