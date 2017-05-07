d7085a4fae84ff7cf107f4aba831a4fd31755508
Mieszkania <mieszkania@users.noreply.github.com>
2017-05-07 21:37:26 +0200

    Make the conversion plugin use config.

--- analytics-conversions-plugin.js
+++ analytics-conversions-plugin.js
@@ -1,11 +1,28 @@
 (function() {
-  var url = 'https://www.googleadservices.com/pagead/conversion';
+  var url = 'https://www.googleadservices.com/pagead/conversion/%id%/' +
+            '?label=%label%&guid=ON&script=0';
+  var configs = {};
 
-  var plugin = function(tracker, config) {};
+  var plugin = function(tracker, config) {
+    configs = config;
+  };
 
-  plugin.prototype.send = function(id, label) {
+  function format(str, replacements) {
+    return str.replace(/%(\w+)%/g, function(ignore, tag) {
+      return replacements[tag] || '';
+    }
+  }
+
+  plugin.prototype.send = function(name) {
+    var config = configs[name];
+    if (!config) {
+      return;
+    }
     var img = new Image(1, 1);
-    img.src = url + '/' + id + '/?label=' + label + '&guid=ON&script=0';
+    img.src = format(url, {
+      'id': config.id,
+      'label': config.label,
+    });
   };
 
   ga('provide', 'conversions', plugin);
