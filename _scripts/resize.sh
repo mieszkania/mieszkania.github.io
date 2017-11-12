2ac59646df0287fc74b00d07ccac94bdf0d8b255
Mieszkania <mieszkania@users.noreply.github.com>
2017-11-12 15:32:30 +0100

    Script to resize and optimize

---
+++ _scripts/resize.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function optimize() {
+  if [[ "${1}" == *.jpg ]]
+  then
+    jpegtran -copy none -optimize -progressive -outfile "${1}.tmp" "${1}"
+    mv "${1}.tmp" "${1}"
+  fi
+  if [[ "${1}" == *.png ]]
+  then
+    optipng -quiet -o2 -strip all "${1}"
+  fi
+}
+
+cd "${1}"
+find "3000" -type f | (
+  while read file
+  do
+    optimize "${file}"
+    base="$(basename "${file}")"
+    for size in 360 480 800 1280 1440 1920
+    do
+      convert "${file}" -resize "${size}x${size}" "${size}/${base}"
+      optimize "${size}/${base}"
+    done
+  done
+)
