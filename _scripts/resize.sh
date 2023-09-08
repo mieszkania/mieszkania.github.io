f562553f3ade2daabd5ccb6620692aebc631da63
Mieszkania <mieszkania@users.noreply.github.com>
2023-09-08 17:35:15 +0200

    Temp change for Mac

--- _scripts/resize.sh
+++ _scripts/resize.sh
@@ -16,12 +16,13 @@ cd "${1}"
 find "3000" -type f | (
   while read file
   do
-    optimize "${file}"
+    #optimize "${file}"
     base="$(basename "${file}")"
     for size in 360 480 800 1280 1440 1920
     do
-      convert "${file}" -resize "${size}x${size}" "${size}/${base}"
-      optimize "${size}/${base}"
+      #convert "${file}" -resize "${size}x${size}" "${size}/${base}"
+      sips -Z "${size}" "${file}" --out "${size}/"
+      #optimize "${size}/${base}"
     done
   done
 )
